af1233821ffcbfe104e3ca9bcc45d5e158e3772a
Kylan Kidd <Kylan Kidd@LAPTOP-N4HJ4EQC>
2022-09-15 09:29:03 -0600

    For Loop Created Created a loop that counts up until I = X.

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -19,8 +19,16 @@ public class HelloWorld {
         
          System.out.println("Hello World!");
          
+        int x = 6;
+        for(int i=0; i<x; i++){
+            System.out.println(i);
+        }
+        }
+        
     }
+    
+   
    
     
     
-}
+
