8e779d3a41f450da154e4f8a81b69d7ebcf749b1
Kylan Kidd <Kylan Kidd@LAPTOP-N4HJ4EQC>
2022-09-15 09:45:24 -0600

    Project Created HelloWorld application.

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -17,12 +17,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         // TODO code application logic here
         
-         System.out.println("Hello World!");
-         
-        int x = 6;
-        for(int i=0; i<x; i++){
-            System.out.println(i);
-        }
+     
         }
         
     }
